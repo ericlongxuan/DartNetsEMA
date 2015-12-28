25f99395e3d24cc8b4c3ec618c04172d7f07c4c1
Eric Wang <weichen.wang@live.com>
2015-12-28 02:51:16 -0500

    change common.js to OOP

--- js/my-site/common.js
+++ js/my-site/common.js
@@ -3,8 +3,8 @@
 
 // when the DOM is loaded
 $(document).ready(function() {
-    compileCommonTpl();
-    bindActions();
+    common.compileTpl();
+    common.bindActions();
 
     var data = sessionStorage.getItem("ema-json");
     if (data === null) {
@@ -17,58 +17,60 @@ $(document).ready(function() {
 
 //**************************************
 // Functions
-function compileCommonTpl() {
-    // load data
-    var data = sessionStorage.getItem("ema-json");
-    // compile the template
-    var source = $("#common-tpl").html();
-    var template = Handlebars.compile(source);
-    // replace the template with data
-    $("body").append(template(JSON.parse(data)));
-}
+var common = {
+        compileTpl: function() {
+            // load data
+            var data = sessionStorage.getItem("ema-json");
+            // compile the template
+            var source = $("#common-tpl").html();
+            var template = Handlebars.compile(source);
+            // replace the template with data
+            $("body").append(template(JSON.parse(data)));
+        },
 
-function bindActions() {
-    // bind actions
-    $("#load-ema-link").click(function() {
-        loadPopUp();
-    });
+        bindActions: function() {
+            // bind actions
+            $("#load-ema-link").click(function() {
+                loadPopUp();
+            });
 
-    $("#file-path-input").change(function() {
-        var filePath = "ema/" + getNameFromSelectFile($("#file-path-input").val());
-        loadJsonFile(filePath);
-    });
-}
+            $("#file-path-input").change(function() {
+                var filePath = "ema/" + getNameFromSelectFile($("#file-path-input").val());
+                loadJsonFile(filePath);
+            });
+        },
 
-function loadJsonFile(file_name) {
-    $(".load-modal").modal('hide');
-    $.ajax({
-        type:"GET",
-        url:file_name,
-        cache:false,  //default is true in GET method.
-    })
-        .done(function(response) {
-            sessionStorage.setItem("ema-json", JSON.stringify(response));
-            location.reload();
-        })
-        .fail(function(){
-            alertPopUp("No such file.\nThis system only works with a json file in the 'ema' folder.");
-        });
-}
+        loadJsonFile: function(file_name) {
+            $(".load-modal").modal('hide');
+            $.ajax({
+                    type: "GET",
+                    url: file_name,
+                    cache: false, //default is true in GET method.
+                })
+                .done(function(response) {
+                    sessionStorage.setItem("ema-json", JSON.stringify(response));
+                    location.reload();
+                })
+                .fail(function() {
+                    alertPopUp("No such file.\nThis system only works with a json file in the 'ema' folder.");
+                });
+        },
 
 
-function alertPopUp(message) {
-    $(".alert-message").html(message);
-    $(".alert-modal").modal('show');
-}
+        alertPopUp: function(message) {
+            $(".alert-message").html(message);
+            $(".alert-modal").modal('show');
+        },
 
-function loadPopUp() {
-    $(".load-modal").modal('show');
-}
+        loadPopUp: function() {
+            $(".load-modal").modal('show');
+        },
 
-function getNameFromSelectFile(file) {
-    // ファイル名のみ取得して表示します
-    var regex = /\\|\\/;
-    var array = file.split(regex);
-    return array[array.length - 1];
-}
-//**************************************
+        getNameFromSelectFile: function(file) {
+            // ファイル名のみ取得して表示します
+            var regex = /\\|\\/;
+            var array = file.split(regex);
+            return array[array.length - 1];
+        },
+    }
+    //**************************************
