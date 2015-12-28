f8857b05a6ab98a9c72f605509d531d2f2aed68e
Eric Wang <weichen.wang@live.com>
2015-12-28 04:05:18 -0500

    basic structure for main panel

--- js/my-site/index.js
+++ js/my-site/index.js
@@ -3,7 +3,7 @@
 
 // when the DOM is loaded
 $(document).ready(function() {
-    compileIndexTpl();
+    index.init();
 });
 //**************************************
 
@@ -11,13 +11,38 @@ $(document).ready(function() {
 
 //**************************************
 // Functions
-function compileIndexTpl() {
-    // load data
-    var data = sessionStorage.getItem("ema-json");
-    // compile the template
-    var source = $("#index-tpl").html();
-    var template = Handlebars.compile(source);
-    // replace the template with data
-    $("body").append(template(JSON.parse(data)));
-}
-//**************************************
+var index = {
+        init: function() {
+            index.compileTpl();
+            index.bindActions();
+        },
+
+        compileTpl: function() {
+            // load data
+            var data = sessionStorage.getItem("ema-json");
+            // compile the template
+            var source = $("#index-tpl").html();
+            var template = Handlebars.compile(source);
+            // replace the template with data
+            $("body").append(template(JSON.parse(data)));
+        },
+
+        bindActions: function() {
+            // bind actions
+            $(".sidebar .nav a").click(function() {
+                index.scrollToDiv($(this).data("href"));
+            });
+
+            $(".main .cat-del-btn").click(function() {
+                common.alertPopUp($(this).data("cat"));
+            });
+        },
+
+        scrollToDiv: function(divId) {
+            $('html, body').animate({
+                scrollTop: $("#" + divId).offset().top - 70
+            }, 300);
+        },
+    }
+    //**************************************
+    
