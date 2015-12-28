64ad0629020d65d761a0ddcb96c4b96c6bb419a7
Eric Wang <weichen.wang@live.com>
2015-12-28 04:04:01 -0500

    file format

--- js/my-site/handlerbars-helper.js
+++ js/my-site/handlerbars-helper.js
@@ -6,22 +6,38 @@ Handlebars.registerHelper('compare', function(lvalue, rvalue, options) {
     var operator = options.hash.operator || "==";
 
     var operators = {
-        '==':       function(l,r) { return l == r; },
-        '===':      function(l,r) { return l === r; },
-        '!=':       function(l,r) { return l != r; },
-        '<':        function(l,r) { return l < r; },
-        '>':        function(l,r) { return l > r; },
-        '<=':       function(l,r) { return l <= r; },
-        '>=':       function(l,r) { return l >= r; },
-        'typeof':   function(l,r) { return typeof l == r; }
+        '==': function(l, r) {
+            return l == r;
+        },
+        '===': function(l, r) {
+            return l === r;
+        },
+        '!=': function(l, r) {
+            return l != r;
+        },
+        '<': function(l, r) {
+            return l < r;
+        },
+        '>': function(l, r) {
+            return l > r;
+        },
+        '<=': function(l, r) {
+            return l <= r;
+        },
+        '>=': function(l, r) {
+            return l >= r;
+        },
+        'typeof': function(l, r) {
+            return typeof l == r;
+        }
     }
 
     if (!operators[operator])
-        throw new Error("Handlerbars Helper 'compare' doesn't know the operator "+operator);
+        throw new Error("Handlerbars Helper 'compare' doesn't know the operator " + operator);
 
-    var result = operators[operator](lvalue,rvalue);
+    var result = operators[operator](lvalue, rvalue);
 
-    if( result ) {
+    if (result) {
         return options.fn(this);
     } else {
         return options.inverse(this);
