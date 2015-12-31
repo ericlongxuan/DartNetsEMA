87739d9e318c7d1551eb96b583e9c054e5851397
Eric Wang <weichen.wang@live.com>
2015-12-31 18:31:17 -0500

    js save file

--- js/my-site/common.js
+++ js/my-site/common.js
@@ -80,9 +80,31 @@ var common = {
             //     });
         },
 
-        saveJsonFile: function(filename) {
+        saveJsonFile: function() {
             var dataJson = sessionStorage.getItem("ema-json");
-            alert(dataJson);
+            window.URL = window.URL || window.webkitURL;
+            var blob = new Blob([dataJson], {
+                type: 'text/plain;charset=utf-8'
+            });
+            var currentdate = new Date();
+            var datetime = currentdate.getDate() + "/" + (currentdate.getMonth() + 1) + "/" + currentdate.getFullYear();
+            filename = 'ema-' + datetime + '.json';
+            var type = blob.type;
+            var force_saveable_type = 'application/octet-stream';
+            if (type && type != force_saveable_type) {
+                var slice = blob.slice || blob.webkitSlice || blob.mozSlice;
+                blob = slice.call(blob, 0, blob.size, force_saveable_type);
+            }
+
+            var url = URL.createObjectURL(blob);
+            var save_link = document.createElementNS('http://www.w3.org/1999/xhtml', 'a');
+            save_link.href = url;
+            save_link.download = filename;
+
+            var event = document.createEvent('MouseEvents');
+            event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+            save_link.dispatchEvent(event);
+            URL.revokeObjectURL(url);
         },
 
 
